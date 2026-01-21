9b71bfe1a52b080122687fef2b088d3fa868c9ed
Dotta1996 <110409024+Dotta1996@users.noreply.github.com>
2026-01-21 20:34:12 -0300

    Update firebase.js

--- js/firebase.js
+++ js/firebase.js
@@ -1,21 +1,16 @@
-import { initializeApp, getApps } from "firebase/app";
+import { initializeApp, getApps } 
+from "https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBkLoi7cu_qLWiPTlHiNdh1-WpHIHrHCnA",
-  authDomain: "controle-financeiro-pess-64800.firebaseapp.com",
-  projectId: "controle-financeiro-pess-64800",
-  storageBucket: "controle-financeiro-pess-64800.firebasestorage.app",
-  messagingSenderId: "704824842865",
-  appId: "1:704824842865:web:2871181735530a720659d0"
+  apiKey: "SUA_API_KEY",
+  authDomain: "SEU_PROJETO.firebaseapp.com",
+  projectId: "SEU_PROJETO",
+  storageBucket: "SEU_PROJETO.appspot.com",
+  messagingSenderId: "XXXX",
+  appId: "XXXX"
 };
 
-// 🔒 Só inicializa se ainda não existir
-const app = getApps().length === 0
-  ? initializeApp(firebaseConfig)
-  : getApps()[0];
-
-export { app };
-
-import { getApps } from "firebase/app";
-
-console.log("Apps Firebase:", getApps());
+export const app =
+  getApps().length === 0
+    ? initializeApp(firebaseConfig)
+    : getApps()[0];
