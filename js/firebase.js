ad5d956c1bc8ef11d7bb07f69099efb77bce28c7
Dotta1996 <110409024+Dotta1996@users.noreply.github.com>
2026-01-21 19:07:54 -0300

    Update firebase.js

--- js/firebase.js
+++ js/firebase.js
@@ -1,1 +1,14 @@
-// firebase.js
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBkLoi7cu_qLWiPTlHiNdh1-WpHIHrHCnA",
+  authDomain: "controle-financeiro-pess-64800.firebaseapp.com",
+  projectId: "controle-financeiro-pess-64800",
+  storageBucket: "controle-financeiro-pess-64800.firebasestorage.app",
+  messagingSenderId: "704824842865",
+  appId: "1:704824842865:web:2871181735530a720659d0"
+};
+
+const app = initializeApp(firebaseConfig);
+export const db = getFirestore(app);
